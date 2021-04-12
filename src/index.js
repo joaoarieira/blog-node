3aa75f2fd45f5a1350bd21a5ce4016f50d47a170
João Azevedo <joao.azevedo@ufv.br>
2021-04-11 22:32:45 -0300

    Novo blog adicionado para testes.

--- src/index.js
+++ src/index.js
@@ -29,7 +29,26 @@ function validateBlogId(request, response, next){
 
 app.use(logRequests);
 app.use('/blogs/:id', validateBlogId);
-const blogs = [{"id": "513eb15b-5975-4188-8dc7-754e2325c59c", "title": "Title lorem ipsum", "snippet": "Snippet lorem ipsum", "text": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium est ducimus dicta recusandae harum neque maxime ullam omnis facere blanditiis dolorem quisquam inventore amet, delectus possimus sapiente veniam voluptas eligendi voluptatem illum? Vitae est dicta perferendis quod inventore officiis similique."}];
+const blogs = [
+    {
+        "id": "513eb15b-5975-4188-8dc7-754e2325c59c", 
+        "title": "Title lorem ipsum", 
+        "snippet": "Snippet lorem ipsum", 
+        "text": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium est ducimus dicta recusandae harum neque maxime ullam omnis facere blanditiis dolorem quisquam inventore amet, delectus possimus sapiente veniam voluptas eligendi voluptatem illum? Vitae est dicta perferendis quod inventore officiis similique."
+    },
+    {
+        "id": "e42afd0f-00e3-4d71-a6d0-f4dec0440d42",
+        "title": "A blog title",
+        "snippet": "This is a blog snippet with small amount of words.",
+        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi autem exercitationem accusantium fuga, corporis, quaerat impedit magnam quia saepe illum consectetur obcaecati reprehenderit unde ad vero? Consectetur itaque necessitatibus temporibus nihil corrupti. Ipsam vitae, nostrum sunt veniam nisi iusto dicta. Corrupti nihil, voluptatum eum perferendis veritatis molestias non qui! Fuga earum possimus officia iste vitae doloremque est, inventore aut esse ut soluta dignissimos expedita blanditiis dolor iusto tenetur voluptas voluptate, eligendi nisi! Vero quam dolorum illum debitis qui ducimus magni aspernatur accusantium eum fugit. Necessitatibus impedit magni, facere rerum esse, natus exercitationem et tempora officia doloribus vitae qui sint dolores quia accusantium similique quo laudantium fugit accusamus. Exercitationem ullam dicta quo laborum quis repellat repellendus voluptatem aliquid rerum, explicabo corrupti amet tenetur perferendis alias quia eum nesciunt quod quibusdam illo officiis maiores reiciendis itaque quidem. Tempore eius modi voluptates ut! Enim eos distinctio praesentium vel error minus, magnam maxime, autem nobis sint saepe pariatur dolorum ut! Delectus modi, veniam, eos recusandae minima dolor incidunt consectetur distinctio deserunt in dolore commodi laborum! Repellendus, labore. Aperiam, soluta! Facere at suscipit impedit eum recusandae ducimus assumenda cum quisquam dolor aut! Sequi ut incidunt est veniam laudantium in qui, obcaecati assumenda impedit corporis! Omnis."
+    },
+    {
+        "id": "e42afd0f-00e3-4d71-a6d0-f4dec0440ds2",
+        "title": "Another blog title",
+        "snippet": "This is a blog snippet with a lot of words. A lot of words. A lot of words. A lot of words. A lot of words. A lot of words.",
+        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi autem exercitationem accusantium fuga, corporis, quaerat impedit magnam quia saepe illum consectetur obcaecati reprehenderit unde ad vero? Consectetur itaque necessitatibus temporibus nihil corrupti. Ipsam vitae, nostrum sunt veniam nisi iusto dicta. Corrupti nihil, voluptatum eum perferendis veritatis molestias non qui! Fuga earum possimus officia iste vitae doloremque est, inventore aut esse ut soluta dignissimos expedita blanditiis dolor iusto tenetur voluptas voluptate, eligendi nisi! Vero quam dolorum illum debitis qui ducimus magni aspernatur accusantium eum fugit. Necessitatibus impedit magni, facere rerum esse, natus exercitationem et tempora officia doloribus vitae qui sint dolores quia accusantium similique quo laudantium fugit accusamus. "
+    }
+];
 
 app.get('/blogs', (request, response) => {
     const { title } = request.query;
@@ -45,7 +64,7 @@ app.post('/blogs', (request, response) => {
     const { title, snippet, text } = request.body; 
 
     const blog = {id: uuidv4(), title, snippet, text};
-    //console.log(blog);
+    console.log(blog);
     blogs.push(blog);
 
     return response.json(blogs);
